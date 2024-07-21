94b2d006cf0fa2363b0da8813cd2c8ca37663e47
Abdulrahuman <132744501+AbdulRahumankm@users.noreply.github.com>
2024-07-21 17:16:39 +0530

    Summary

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
-import { useLazyGetSummaryQuery } from "../services/article.js";
+import { useLazyGetSummaryQuery } from "../services/article";
+
 
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -79,7 +80,7 @@ const Demo = () => {
           {allArticles.map((item, index) => (
             <div
               key={`link-${index}`}
-              onClick={() =>setArticle(item)}
+              onClick={() => setArticle(item)}
               className="link_card"
             >
               <div className="copy_btn">
@@ -89,12 +90,35 @@ const Demo = () => {
                   className="w-[40%] h-[40%] object-contain"
                 />
               </div>
-              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>{item.url}</p>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
             </div>
           ))}
         </div>
       </div>
       {/* Display Result */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFatching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happan...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+              Article<span className="blue_gradient">Summary</span></h2>
+              <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
